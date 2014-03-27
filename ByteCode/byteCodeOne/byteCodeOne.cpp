028710f97d90b244a93e9462218eb193e7c2f5a0
Dafmulder <dafmulder@gmail.com>
2014-03-27 22:17:14 +0100

    Initial commit

---
+++ ByteCode/byteCodeOne/byteCodeOne.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : byteCodeOne.cpp
+// Author      : Douwe Bart Mulder
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello, you've been RAQ3D!" << endl; // prints Hello, you've been RAQ3D!
+	return 0;
+}
